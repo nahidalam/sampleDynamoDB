2b62f3a87d611c097f6b04037bebed349f13a4dc
nahidalam <shapla@gmail.com>
2017-08-14 18:30:19 -0700

    Updating followup page

--- js/decisions.js
+++ js/decisions.js
@@ -3,50 +3,9 @@ $( document ).ready(function() {
 	//var socket = io.connect('http://www.ciscochill.com:80');
 	var socket = io.connect('http://localhost:8080');
 
-  socket.on('card', function (message) {
+	$("#clickFollowup").click(function(){
+		//when a checkbox in one of the followup is clicked
 
-    var image = $("#img");
-		console.log("received card");
+	})
 
-    image.fadeOut('fast', function () {
-			if (message == 0 ){
-        image.attr('src', '/img/cards/ace_of_spades.png');
-			}
-			else if (message == 1 ){
-        image.attr('src', '/img/cards/king_of_spades2.png');
-			}
-			else if (message == 2 ){
-        image.attr('src', '/img/cards/queen_of_spades2.png');
-			}
-			else if (message == 3 ){
-        image.attr('src', '/img/cards/jack_of_spades2.png');
-			}
-			else if (message == 4 ){
-        image.attr('src', '/img/cards/10_of_spades.png');
-			}
-        image.fadeIn('fast');
-    });
-  });
-
-  socket.on('finger', function (message) {
-    var image = $("#img");
-
-    image.fadeOut('fast', function () {
-				image.attr('src', '/img/ABC.jpg');
-        image.fadeIn('fast');
-    });
-  });
-
-  socket.on('flash', function (message) {
-		console.log("received flash");
-    $("#alarm").fadeIn(3000);
-    $("#alarm").fadeOut(2000);
-  });
-
-	socket.on('reset', function (message) {
-			//clear up the page
-			console.log("received reset reply");
-			$("#img").hide();
-  });
-
-});
+  
--- js/followup.js
+++ js/followup.js
@@ -2,133 +2,32 @@ $( document ).ready(function() {
 
 	//var socket = io.connect('http://www.ciscochill.com:80');
 	var socket = io.connect('http://localhost:8080');
-	console.log (socket);
-	socket.emit("reset","all");
 
-	var mySwiper = new Swiper ('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      freeModeMomentum: true
-    })
+	//read from database and create based on questions whose decision has been taken already
+	var optionValues= [[1,"first"],[2,"second"]];
+
+  jQuery("#populateMenu").click( function () {
+        for (var i=0;i<optionValues.length;i++){
+            jQuery('#quesSelect').append(jQuery("<option></option>").val(optionValues[i][0]).text(optionValues[i][1]))
+        }
+    });
 
 	$("#select").click(function(){
+		//what happens when question arrow drag&drop clicked?
+		//reads all questions whose decision has been taken and shows them
 
-	//turn of card select
-	mySwiper.lockSwipes();
-
-	//turn it back on, if needed.
-	//mySwiper.unlockSwipes()
-
-	//This always holds the index of the current card.
-	//console.log(mySwiper.realIndex);
-
-	document.getElementById("btnLocal").style.display="inline-block";
-	document.getElementById("btnDist").style.display="inline-block";
-	document.getElementById("btnGT").style.display="inline-block";
-	document.getElementById("select").style.display="none";
+	})
+	$("#selectQuestion").click(function(){
+		//what happens when one of the questions is selected
+		//reads its decision from decision table and shows in a div
+		//also creates a space for followup writing
 
 	})
 
-	$("#btnLocal").click(function(){
-		document.getElementById("btnLocal").style.display="none";;
-		document.getElementById("btnDist").style.display="none";
-		document.getElementById("btnGT").style.display = "none";
-		document.getElementById("select").style.display="none";
-		document.getElementById("btnReSelectLocal").style.display="inline-block";
-		document.getElementById("btnReSelectDist").style.display="none";
-		document.getElementById("btnReSelectGT").style.display="none";
+
+	$("#btnFollowupSubmit").click(function(){
+		//gather all the data for the Followup Table, create a JSON and send it
+		socket.emit('insertFollowup',tableJSON);
+		console.log ("sent database insert request at Followup Table" + index);
 
 	})
-
-	$("#btnDist").click(function(){
-
-		document.getElementById("btnLocal").style.display="none";;
-		document.getElementById("btnDist").style.display="none";
-		document.getElementById("btnGT").style.display = "none";
-		document.getElementById("select").style.display="none";
-		document.getElementById("btnReSelectLocal").style.display="none";
-		document.getElementById("btnReSelectDist").style.display="inline-block";
-		document.getElementById("btnReSelectGT").style.display="none";
-		//socket.emit('card',mySwiper.realIndex);
-		var index = mySwiper.realIndex;
-		//var index = 1;
-		//socket.emit('message',index);
-		socket.emit('card',index);
-		console.log ("sent card index:" + index);
-
-	})
-	$("#btnGT").click(function(){
-
-		console.log ("Hybrid Selected");
-		document.getElementById("btnLocal").style.display="none";
-		document.getElementById("btnDist").style.display="none";
-		document.getElementById("btnGT").style.display = "none";
-		document.getElementById("select").style.display="none";
-		document.getElementById("btnReSelectLocal").style.display="none";
-		document.getElementById("btnReSelectDist").style.display="none";
-
-		/*$(".button").fadeOut('fast',function(){
-			$("#btnReSelectGT").fadeIn();
-		});*/
-
-		document.getElementById("btnReSelectGT").style.display="inline-block";
-		socket.emit('finger','ABC');
-		console.log ("finger sent");
-
-		//show card and ABC.png here
-
-
-		/*$( "#content").animate({
-    width: "50%"
-  	}, 5000, function() {
-    // Animation complete.
-  });*/
-	//.swiper-container
-
-	$( ".cardimage").animate({
-	//width: "50%"
-	height: "50%"
-	//left: "-=10%"
-}, 3000, function() {
-	// Animation complete.
-	//$("#fingerprint").fadeIn(3000);
-	var image = $("#fingerprint");
-	//image.attr('left', '400');
-	image.fadeIn('fast');
-	//image.attr('src', '/img/ABC.jpg');
-	//image.attr('height', '726');
-	//image.attr('width', '500');
-	//image.fadeIn('fast');
-
-	/*image.fadeOut('fast', function () {
-			image.attr('src', '/img/ABC.jpg');
-			image.attr('height', '726');
-			image.attr('width', '500');
-			image.fadeIn('fast');
-	});*/
-});
-	})
-
-	$("#btnReSelectLocal").click(function(){
-
-		console.log("Local Reselected");
-		mySwiper.unlockSwipes();
-		document.getElementById("btnReSelectLocal").style.display="none";
-	})
-	$("#btnReSelectDist").click(function(){
-		console.log("Distributed reselected");
-		socket.emit('flash',"flashing");
-		document.getElementById("btnReSelectDist").style.display="none";
-	})
-	$("#btnReSelectGT").click(function(){
-		mySwiper.unlockSwipes();
-		var image = $("#fingerprintXYZ");
-		//image.attr('src', '/img/XYZ.jpg');
-		image.fadeIn('fast');
-		console.log("Hybrid reselected");
-		socket.emit('flash',"flashing");
-		//data and signature will change here
-		document.getElementById("btnReSelectGT").style.display="none";
-	})
-
-});
