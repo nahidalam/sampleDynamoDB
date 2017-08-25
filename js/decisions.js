3da31df2f3900d0b8a916fa7a2d35a97a0b8dd95
nahidalam <shapla@gmail.com>
2017-08-24 18:48:26 -0700

    Shows decision table wo header

--- js/decisions.js
+++ js/decisions.js
@@ -3,9 +3,34 @@ $( document ).ready(function() {
 	//var socket = io.connect('http://www.ciscochill.com:80');
 	var socket = io.connect('http://localhost:8080');
 
-	$("#clickFollowup").click(function(){
-		//when a checkbox in one of the followup is clicked
+	var tableNameDecision = "Decision"
+	var fields = ['a','b','c'];
+	var data = ['p', 'q', 'r'];
+	var who = ['x', 'y', 'z'];
 
-	})
 
-  
+
+	$(function() {
+    var table = $("#resultTable");
+    var rowNum = 3;
+    var resultHtml = '';
+
+	for(var i = 0 ; i < rowNum ; i++) {
+		resultHtml += ["<tr>",
+	 "<td>",
+	  fields[i],
+	 "</td>",
+	 "<td>",
+	  data[i],
+	 "</td>",
+	 "<td>",
+	  who[i],
+	 "</td>",
+	 '</tr>'].join("\n");
+	}
+
+	table.html(resultHtml);
+    return false;
+});
+
+});
