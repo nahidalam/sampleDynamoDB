e27816fbf6c1ec58a868dff3ea9de13fdd768fc7
nahidalam <shapla@gmail.com>
2017-09-14 10:56:45 -0700

    update

--- js/create_new_ques.js
+++ js/create_new_ques.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
   $('#sliderImage').prepend('<img id="sliderImg" src="Images/ic_slider.png" />')
   $('#shannonImage').prepend('<img id="shannonImg" src="Images/ic_shannon@2x.png" />')
   $('#aliceImage').prepend('<img id="aliceImg" src="Images/ic_alice@2x.png" />')
+  $('#whitImage').prepend('<img id="whitImg" src="Images/ic_whit@2x.png" />')
+  $('#sunilImage').prepend('<img id="sunilImg" src="Images/ic_sunil@2x.png" />')
 
   var name = "Justin" //will be passed from the login page through socket eventually
 
---
+++ js/test.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#shannonImage').prepend('<img id="shannonImg" src="Images/ic_shannon@2x.png" />')
+  $('#aliceImage').prepend('<img id="aliceImg" src="Images/ic_alice@2x.png" />')
+  $('#whitImage').prepend('<img id="whitImg" src="Images/ic_whit@2x.png" />')
+  $('#sunilImage').prepend('<img id="sunilImg" src="Images/ic_sunil@2x.png" />')
+
+});
