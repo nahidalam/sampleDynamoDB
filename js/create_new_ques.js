698339c07a186805c801765784fa5b0c6bf3488c
nahidalam <shapla@gmail.com>
2017-09-14 17:24:37 -0700

    update
    Proper color and participant spacing, Botton edge rounded

--- js/create_new_ques.js
+++ js/create_new_ques.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   $('#aliceImage').prepend('<img id="aliceImg" src="Images/ic_alice@2x.png" />')
   $('#whitImage').prepend('<img id="whitImg" src="Images/ic_whit@2x.png" />')
   $('#sunilImage').prepend('<img id="sunilImg" src="Images/ic_sunil@2x.png" />')
-
+  $('#nahidImage').prepend('<img id="nahidImg" src="Images/ic_nahid@2x.png" />')
   var name = "Justin" //will be passed from the login page through socket eventually
 
   $("#welcomeText").append(  name );
